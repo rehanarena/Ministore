cc07cd946d367285f2e021a4467cfcf28959ba18
Rehana <nfsrehana@gmail.com>
2024-04-20 20:33:07 +0530

    wishlistAdded

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../model/userSchema");
 const order = require("../model/orderSchema");
+const WishList = require("../model/wishlistSchema");
 
 const { isLoggedIn } = require("../middlewares/authMiddleware");
 const userController = require("../controller/userController");
@@ -40,6 +41,20 @@ router
   .route("/address/delete-address/:id")
   .delete(userController.deleteAddress);
 
+
+
+
+  /**
+ * User Wishlist
+ */
+
+router.get("/wishlist", userController.getWishlist);
+router.post("/add-to-wishlist", userController.addToWishlist);
+router.delete("/remove-from-wishlist", userController.removeFromWishlist);
+
+
+
+
 /**
  * User Order Management
  */
@@ -49,6 +64,21 @@ router.post("/place-order", orderController.placeOrder);
 router.route("/orders").get(orderController.getUserOrders);
 router.get("/order/:id", orderController.getUserOrder);
 router.post("/cancel-order/:id", orderController.cancelOrder);
+
+
+// router.route("/cancel-order/:id").post(orderController.cancelOrder)
+
+
+
+
+
+/**
+ * User Refferals
+ */
+
+router.get("/refferals", userController.getRefferals);
+
+
 
 
 
